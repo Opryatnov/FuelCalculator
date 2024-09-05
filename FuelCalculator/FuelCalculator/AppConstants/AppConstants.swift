b03284de0103a949e9da04b92bdc254f5ee88811
Dmitriy Opryatnov <dmitry.opriatnov@fmobile.by>
2024-09-05 14:21:13 +0300

    Изменил ссылку с беларусь нефти на А100 изменил работу парссера

--- FuelCalculator/FuelCalculator/AppConstants/AppConstants.swift
+++ FuelCalculator/FuelCalculator/AppConstants/AppConstants.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 enum AppConstants {
-    static let fuelListRequest: String = "https://azs.belorusneft.by/sitebeloil/ru/center/azs/center/fuelandService/price/"
+    static let fuelListRequest: String = "https://azs.a-100.by/"
+//    "https://azs.belorusneft.by/sitebeloil/ru/center/azs/center/fuelandService/price/"
     static let googleVideoADKey: String = "ca-app-pub-6665857691249161/2787908576"
     static let googleBannerADKey: String = "ca-app-pub-6665857691249161/8273799852"
 }
--- FuelCalculator/FuelCalculator/UI/FuelCalculatorViewController/HTMLParser.swift
+++ FuelCalculator/FuelCalculator/UI/FuelCalculatorViewController/HTMLParser.swift
@@ -15,36 +15,39 @@ final class HTMLParser {
             fuel = []
             let document: Document = try SwiftSoup.parse(html)
             guard let body = document.body() else { return }
-            let th: Elements? = try body.select("th")
-            let td: Elements? = try body.select("td")
+            let th: Elements? = try body.select("div")
+            let td: Elements? = try body.select("div")
+            let span: Elements? = try body.select("span")
             th?.enumerated().forEach { index, value in
                 let fuelName = try? value.text()
-                if fuelName == "Автогаз (ПБА)*" {
-                    let valueAmount = try? td?[index].text()
-                    let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "Gas", fuelName_ENG: "Gas: propane-butane", fuelName_BEL: "Газ: прапан-бутан")
-                    fuel?.append(tempFuel)
-                } else if fuelName == "ДТ" {
-                    let valueAmount = try? td?[index].text()
+                if fuelName?.contains("Газ") == true {
+                    if let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: "."),
+                       Double(valueAmount) != nil {
+                        let tempFuel = Fuel(name: "Газ: пропан - бутан", amount: valueAmount, fuelCode: "Gas", fuelName_ENG: "Gas: propane-butane", fuelName_BEL: "Газ: прапан-бутан")
+                        fuel?.append(tempFuel)
+                    }
+                } else if fuelName == "ДТ Евро" {
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "DT", fuelName_ENG: "Diesel fuel", fuelName_BEL: "Дызельнае паліва")
                     fuel?.append(tempFuel)
-                } else if fuelName == "ДТ ECO" {
-                    let valueAmount = try? td?[index].text()
+                } else if fuelName == "AdBlue" {
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "DT-ECO", fuelName_ENG: "Diesel fuel (ECO)", fuelName_BEL: "Дызельнае паліва (ECO)")
                     fuel?.append(tempFuel)
                 } else if fuelName == "ДТЗ -32°" {
-                    let valueAmount = try? td?[index].text()
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "DT-Winter", fuelName_ENG: "Diesel fuel, winter (-32)", fuelName_BEL: "Дызельнае паліва, зімовае (-32)")
                     fuel?.append(tempFuel)
                 } else if fuelName == "АИ-92" {
-                    let valueAmount = try? td?[index].text()
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "AI-92", fuelName_ENG: "Gasoline AI - 92", fuelName_BEL: "Бензін АІ - 92")
                     fuel?.append(tempFuel)
                 } else if fuelName == "АИ-95" {
-                    let valueAmount = try? td?[index].text()
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "AI-95", fuelName_ENG: "Gasoline AI - 95", fuelName_BEL: "Бензін АІ - 95")
                     fuel?.append(tempFuel)
                 } else if fuelName == "АИ-98" {
-                    let valueAmount = try? td?[index].text()
+                    let valueAmount = try? td?[index + 1].text().replacingOccurrences(of: ",", with: ".")
                     let tempFuel = Fuel(name: fuelName, amount: valueAmount, fuelCode: "AI-98", fuelName_ENG: "Gasoline AI - 98", fuelName_BEL: "Бензін АІ - 98")
                     fuel?.append(tempFuel)
                 }
