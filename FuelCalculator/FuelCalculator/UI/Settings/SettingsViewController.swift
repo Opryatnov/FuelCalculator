e41c055a8eee78f917519068a32cca97be3d1212
Dmitriy Opryatnov <dmitry.opriatnov@fmobile.by>
2024-09-13 09:07:34 +0300

    Approved build

--- FuelCalculator/FuelCalculator/UI/Settings/SettingsViewController.swift
+++ FuelCalculator/FuelCalculator/UI/Settings/SettingsViewController.swift
@@ -135,7 +135,7 @@ final class SettingsViewController: UIViewController {
     }
     
     private func shareIt() {
-        guard let urlToShare = URL(string: "https://testflight.apple.com/join/VUzxDD51") else { return }
+        guard let urlToShare = URL(string: "https://apps.apple.com/by/app/%D0%BA%D0%B0%D0%BB%D1%8C%D0%BA%D1%83%D0%BB%D1%8F%D1%82%D0%BE%D1%80-%D1%82%D0%BE%D0%BF%D0%BB%D0%B8%D0%B2%D0%B0-%D1%80%D0%B1/id6636476826") else { return }
         let textToShare = LS("FUEL.CALCULATOR.TAB.TITLE")
         
         let itemsToShare = [textToShare, urlToShare] as [Any]
